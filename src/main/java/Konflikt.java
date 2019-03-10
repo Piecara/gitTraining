04c947baf1ab471524dc72b6b1d436d032bfbe63
Piecara <dominikwawrzkiewicz141@gmail.com>
2019-03-10 22:23:43 +0100

    klasa konflikt

---
+++ src/main/java/Konflikt.java
@@ -0,0 +1,22 @@
+public class Konflikt {
+    public static void main(String[] args) {
+        System.out.println("print print ");
+        System.out.println("print print ");
+        System.out.println("print print ");
+        System.out.println("print print ");
+        System.out.println("print print ");
+        System.out.println("print print ");
+        System.out.println("print print ");
+        System.out.println("print print ");
+        System.out.println("print print ");
+        System.out.println("print print ");
+        System.out.println("print print ");
+        System.out.println("print print ");
+        System.out.println("print print ");
+        System.out.println("print print ");
+        System.out.println("print print ");
+        System.out.println("print print ");
+        System.out.println("print print ");
+        System.out.println("print print ");
+    }
+}
